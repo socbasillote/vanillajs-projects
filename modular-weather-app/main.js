bb17182a97cd3b5881e3455113ae02cb157d08e2
Soc Basillote <dicecepe@gmail.com>
2025-05-12 08:00:54 +0800

    update for modular weather

---
+++ modular-weather-app/main.js
@@ -0,0 +1,62 @@
+import { getWeather } from "./weather/api.js";
+import { renderWeather } from "./weather/dom.js";
+
+const btn = document.getElementById('get-weather');
+const input = document.getElementById('city-input');
+
+const historyKey = "weatherSearchHistory";
+const historyList = document.getElementById("history-list");
+
+let currentUnit = "metric";
+
+const celsiusBtn = document.getElementById("celsius-btn");
+const fahrenheitBtn = document.getElementById("fahrenheit-btn");
+
+celsiusBtn.addEventListener("click", () => {
+    currentUnit = "metric";
+    celsiusBtn.classList.add("active");
+    fahrenheitBtn.classList.remove("active");
+})
+
+fahrenheitBtn.addEventListener("click", () => {
+    currentUnit = "imperial";
+    fahrenheitBtn.classList.add("active");
+    celsiusBtn.classList.remove("acitve");
+})
+
+function saveToHistory(city) {
+    let history = JSON.parse(localStorage.getItem(historyKey)) || [];
+    history = history.filter(item => item !== city); // Remove duplicates
+    history.unshift(city); // add to top
+    if (history.length > 5) history.pop(); // Limit to 5 entries
+    localStorage.setItem(historyKey, JSON.stringify(history));
+    renderHistory();
+}
+
+function renderHistory() {
+    let history = JSON.parse(localStorage.getItem(historyKey)) || [];
+    historyList.innerHTML = "";
+    history.forEach(city => {
+        const li = document.createElement("li");
+        li.textContent = city;
+        li.className = "history-item";
+        li.addEventListener("click", () => {
+            getWeather(city);
+        });
+        historyList.appendChild(li);
+    });
+}
+
+btn.addEventListener('click', async () => {
+    const city = input.value.trim();
+    if (city) {
+        const data = await getWeather(city, currentUnit);
+        renderWeather(data);
+        saveToHistory(city);
+    }
+})
+
+
+// Initialize
+
+renderHistory();
